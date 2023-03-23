ff0378bd522a4c05aeea96db0536dfb7c1dd3b9c
Rizki <muhammadrizkitkj123@gmail.com>
2023-03-23 10:54:53 +0700

    create url

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\ModelController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/get', [ModelController::class, 'index']);
+Route::post('/create/todolist', [ModelController::class, 'store']);
+
